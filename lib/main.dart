866b2e8010045cd8bd9c376e81b511608372d889
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-07-01 10:28:07 +0500

    Firebase Integrated

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+ import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized( );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   FirebaseAuth auth = FirebaseAuth.instance;
   User? user = auth.currentUser;
   runApp(MyApp());
